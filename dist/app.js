f133dbb74a91a2e14a245f50320ed55292043d75
dbzguy56 <dbzguy56@gmail.com>
2018-12-04 22:58:44 -0500

    Add update and create forms

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,55 @@
 let app = require('express')();
 let http = require('http').Server(app);
 let path = require('path');
+let bodyParser = require('body-parser');
+const { check, validationResult } = require('express-validator/check');
+// View engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../views'));
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// Global Variables
+app.use((req, res, next) => {
+    res.locals.errors = null;
+    next();
+});
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/index.html'));
+    res.render('index');
+});
+app.route('/update')
+    .get((req, res) => {
+    res.render('update');
+})
+    .put((req, res) => {
+    //
+});
+app.route('/create')
+    .get((req, res) => {
+    res.render('create');
+})
+    .post([check('first_name').isLength({ min: 1 }).withMessage("First Name is required!"),
+    check('last_name').isLength({ min: 1 }).withMessage("Last Name is required!"),
+    check('email').isLength({ min: 1 }).withMessage("Email is required!"),
+    check('email').isEmail().withMessage("Email has to be in the format of email@example.com"),
+    check('favorite_color').isLength({ min: 1 }).withMessage("Favorite color is required!")
+], (req, res) => {
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.render('create', {
+            errors: errors.array()
+        });
+    }
+    else {
+        let newUser = {
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email,
+            favorite_color: req.body.favorite_color,
+        };
+        console.log('New user created!');
+        console.log(newUser);
+    }
 });
 http.listen(3000, () => {
     console.log('Listening on *:3000');
