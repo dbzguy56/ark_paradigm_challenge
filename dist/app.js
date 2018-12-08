60540ca312253e8c0a3f173bf487e3f82f9603ca
dbzguy56 <dbzguy56@gmail.com>
2018-12-08 15:28:06 -0500

    Update textarea to use ajax

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,11 @@
-let app = require('express')();
+let express = require('express');
+let app = express();
 let http = require('http').Server(app);
 let path = require('path');
 let bodyParser = require('body-parser');
 let fs = require("fs");
 let content = require('../person_data.json');
+let io = require('socket.io')(http);
 const { check, validationResult } = require('express-validator/check');
 // View engine
 app.set('view engine', 'ejs');
@@ -11,49 +13,34 @@ app.set('views', path.join(__dirname, '../views'));
 // Body Parser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(express.static('/public'))
+app.use(express.static('public'));
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 // Global Variables
 app.use((req, res, next) => {
     res.locals.errors = null;
     next();
 });
-app.route('/')
-    .get((req, res) => {
+app.get('/', (req, res) => {
     res.render('index', {
         json_data: content
     });
-})
-    .post(check('json_textarea').isLength({ min: 1 }).withMessage("The json file cannot be empty!"), (req, res) => {
-    let errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        res.render('index', {
-            errors: errors.array(),
-            json_data: content
-        });
+});
+app.post('/update', (req, res) => {
+    let jsonMsg = "You have entered invalid JSON!";
+    let status = 400; // Bad request
+    try {
+        let data = req.body;
+        let dataToSave = JSON.stringify(data, null, 2);
+        fs.writeFileSync('person_data.json', dataToSave);
+        jsonMsg = "Your JSON file has succesfully been updated!";
+        content = data;
+        io.emit('update message', dataToSave);
+        status = 200; // OK
     }
-    else {
-        let jsonMsg = "You have entered invalid JSON!";
-        try {
-            let data = JSON.parse(req.body.json_textarea);
-            let dataToSave = JSON.stringify(data, null, 2);
-            fs.writeFileSync('person_data.json', dataToSave);
-            jsonMsg = "Your JSON file has succesfully been updated!";
-            content = data;
-            res.render('index', {
-                json_data: content,
-                jsonMsg: jsonMsg
-            });
-        }
-        catch (_a) {
-            res.render('index', {
-                json_data: content,
-                jsonMsg: jsonMsg
-            });
-        }
-        //content = newUser
-        //let data = JSON.stringify(newUser, null, 2)
-        //fs.writeFileSync('person_data.json', data)
-    }
+    catch (_a) { }
+    res.status(status).json({
+        jsonMsg: jsonMsg
+    });
 });
 app.post('/find', check('key').isLength({ min: 1 }).withMessage("The key is required!"), (req, res) => {
     let errors = validationResult(req);
@@ -64,7 +51,7 @@ app.post('/find', check('key').isLength({ min: 1 }).withMessage("The key is requ
         });
     }
     else {
-        let keyMsg = "The key could not be found!";
+        let keyMsg = "The key '" + req.body.key + "' could not be found!";
         if (content.hasOwnProperty(req.body.key)) {
             keyMsg = "The value for the Key: '" + req.body.key + "' is '" + content[req.body.key] + "'";
         }
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
-let app = require('express')()
+let express = require('express')
+let app = express()
 let http = require('http').Server(app)
 let path = require('path')
 let bodyParser = require('body-parser')
 let fs = require("fs")
 let content = require('../person_data.json')
+let io = require('socket.io')(http)
 
 const {check, validationResult} = require('express-validator/check')
 
@@ -15,7 +17,9 @@ app.set('views', path.join(__dirname, '../views'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-//app.use(express.static('/public'))
+app.use(express.static('public'));
+
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 
 // Global Variables
 app.use((req, res, next) => {
@@ -23,50 +27,34 @@ app.use((req, res, next) => {
   next()
 })
 
-app.route('/')
-  .get((req, res) => {
-    res.render('index', {
-      json_data: content
-    })
+app.get('/', (req, res) => {
+  res.render('index', {
+    json_data: content
   })
-  .post(
-    check('json_textarea').isLength({ min: 1 }).withMessage("The json file cannot be empty!"),
-    (req, res) => {
-    let errors = validationResult(req)
+})
 
-    if (!errors.isEmpty()) {
-      res.render('index', {
-        errors: errors.array(),
-        json_data: content
-      })
-    }
-    else {
-      let jsonMsg = "You have entered invalid JSON!"
-      try {
-        let data = JSON.parse(req.body.json_textarea)
-        let dataToSave = JSON.stringify(data, null, 2)
-        fs.writeFileSync('person_data.json', dataToSave)
-        jsonMsg = "Your JSON file has succesfully been updated!"
+app.post('/update',
+  (req, res) => {
 
-        content = data
-        res.render('index', {
-          json_data: content,
-          jsonMsg: jsonMsg
-        })
-      }
-      catch {
-        res.render('index', {
-          json_data: content,
-          jsonMsg: jsonMsg
-        })
-      }
+  let jsonMsg = "You have entered invalid JSON!"
+  let status = 400 // Bad request
+  try {
+    let data = req.body
+    let dataToSave = JSON.stringify(data, null, 2)
+    fs.writeFileSync('person_data.json', dataToSave)
+    jsonMsg = "Your JSON file has succesfully been updated!"
+    content = data
 
+    io.emit('update message', dataToSave)
 
-      //content = newUser
-      //let data = JSON.stringify(newUser, null, 2)
-      //fs.writeFileSync('person_data.json', data)
-    }
+    status = 200 // OK
+  }
+  catch {}
+
+  res.status(status).json({
+    jsonMsg: jsonMsg
   })
+})
 
 app.post('/find',
   check('key').isLength({ min: 1 }).withMessage("The key is required!"),
@@ -80,7 +68,7 @@ app.post('/find',
       })
     }
     else {
-      let keyMsg = "The key could not be found!"
+      let keyMsg = "The key '" + req.body.key + "' could not be found!"
       if (content.hasOwnProperty(req.body.key)){
         keyMsg = "The value for the Key: '" + req.body.key + "' is '" + content[req.body.key] + "'"
       }
